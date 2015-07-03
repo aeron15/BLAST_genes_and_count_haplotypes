1de27fae3c2e0b4cfd91e42e9fcbcc727572bb98
renan_macbookair <renan.escalante.chong@gmail.com>
2015-07-03 01:59:45 -0400

    Commit haplotype clusters

--- run_BLAST.m
+++ run_BLAST.m
@@ -6,7 +6,7 @@ path_data='/Users/RenanEscalante/Dropbox/Phenotypic_diversity/var_bioinfo/201411
 
 load('targetStrains_WashU.mat')
 
-targetStrains_WashU={'YPS163'};
+%targetStrains_WashU={'YPS163'};
 
 blast_queries_to_targets_extract_ORF(queryGenes,targetStrains_WashU,path_data);
 
