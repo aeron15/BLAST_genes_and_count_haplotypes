67c8d40637f6cf5a0ca94d05dfb220ff1c2f5e6a
renan_macbookair <renan.escalante.chong@gmail.com>
2015-07-21 16:34:29 -0400

    Extract sequences of the Skelly collection

--- extract_sequence_Skelly.py
+++ extract_sequence_Skelly.py
@@ -3,16 +3,14 @@
 import sys, os
 
 """
-Extract specific features from the Skelly genome sequencing
+Extract specific features from the Skelly genome sequencing, specifically GAL3 with upstream sequences 500 base pairs
 
-command11="\"cut -f4,6,10 " + path + "tmp2_"+fastq+"_chr"+ str(i) +".txt>" + path + fastq+"_chr"+ str(i) + ".readsCIGAR\""
-
-bedtools_command="bedtools getfasta -fi YS2.fa -bed GAL3_500bp_upstream.bed  -fo test2.fa.out"
-os.system(bedtools_command)
-bedtools_command="bedtools getfasta -fi " +path + "YS2.fa -bed " + path + " GAL3_500bp_upstream.bed  -fo " +path " test2.fa.out"
 """
+
+allFiles=['273614N.fa','378604X.fa','BC187.fa','DBVPG1106.fa','DBVPG1373.fa','DBVPG6765.fa','K11.fa','L_1374.fa','NCYC361.fa','SK1.fa','UWOPS05_217_3.fa','UWOPS05_227_2.fa','UWOPS83_787_3.fa','UWOPS87_2421.fa','Y12.fa','Y55.fa','YJM975.fa','YJM978.fa','YJM981.fa','YPS128.fa','YPS606.fa','YS2.fa','YS9.fa']
+
 path="/Users/RenanEscalante/Dropbox/Phenotypic_diversity/var_bioinfo/20150324_Skelly_Sequences/"
 
-bedtools_command="bedtools getfasta -fi " +path + "YS2.fa -bed " + path + "GAL3_500bp_upstream.bed  -fo " + path + "test2.fa.out"
-
-print bedtools_command
+for fasta in allFiles:
+	bedtools_command="bedtools getfasta -fi " +path + fasta + " -bed " + path + "GAL3_500bp_upstream.bed  -fo " + path +  fasta +".GAL3_500bp_upstream.out"
+	os.system(bedtools_command)
