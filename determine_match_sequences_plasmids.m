6ae516a058457673417be062734a24f52956cc8d
renan_macbookair <renan.escalante.chong@gmail.com>
2015-07-20 23:31:46 -0400

    Added possibility of blasting a whole folder and expoting result

--- determine_match_sequences_plasmids.m
+++ determine_match_sequences_plasmids.m
@@ -10,18 +10,24 @@ PlasmidData_path='/Users/RenanEscalante/Dropbox/Phenotypic_diversity/var_cloning
 %Path to the data and BLAST location
 path_data='/Users/RenanEscalante/Dropbox/Phenotypic_diversity/var_bioinfo/20141115_BLAST/';
 
-specificPlasmid='RB45';
+specificPlasmid='*';%* blasts every sequence in the folder
 
 files_to_blast=extract_files_to_blast(PlasmidData_path,'specificPlasmid', specificPlasmid);
 
 for iFile=1:length(files_to_blast)
     
-    [allBlastResults_sorted,bestScore_counts] = blast_sequence(files_to_blast{iFile},path_data,PlasmidData_path);
-
     %Remove extension of blasted files
     outputFile_name=remove_extension(files_to_blast{iFile});
-     
-    save(['../output_bioinformatics/BLASTA_scores_'  outputFile_name], 'allBlastResults_sorted','bestScore_counts');
+    
+    try% blasting
+    
+    [allBlastResults_sorted,bestScore_counts] = blast_sequence(files_to_blast{iFile},path_data,PlasmidData_path);     
+    save(['../output_bioinformatics/BLAST_'  outputFile_name], 'allBlastResults_sorted','bestScore_counts');%previously BLASTA
+    
+    catch% catch blast not working
+        display (['BLAST failed with ' outputFile_name]);
+        
+    end
     
   end
 
