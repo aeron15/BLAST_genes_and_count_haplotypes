76a2bcc1d1822bb5aad163cffeb30c58223dca41
renan_macbookair <renan.escalante.chong@gmail.com>
2015-07-31 12:20:24 -0400

    Add blast plasmids results

---
+++ blast_specific_plasmid.m
@@ -0,0 +1,28 @@
+function blast_specific_plasmid (files_to_blast)
+%BLAST_SPECIFC_PLASMID blasts all specific plasmids
+
+for iFile=1:length(files_to_blast)
+        
+        %Remove extension of blasted files
+        outputFile_name=remove_extension(files_to_blast{iFile});
+        
+        try% blasting
+            
+            [allBlastResults_sorted,bestScore_hits{iFile}] = blast_sequence(files_to_blast{iFile},path_data,PlasmidData_path);
+            specificPlasmid_results(iFile).Sequence = files_to_blast{iFile};
+            specificPlasmid_results(iFile).Result = allBlastResults_sorted;
+            
+        catch% catch blast not working
+            display (['BLAST failed with ' outputFile_name]);
+            
+        end
+        
+    end
+        
+    %Determine common best hits across BLASTS. Use when there are several
+    %plasmid sequences
+    commonHits = determine_commonHits_across_BLASTs(bestScore_hits);
+    
+    save(['../output_bioinformatics/BLAST_'  specificPlasmid], 'specificPlasmid_results','bestScore_hits','commonHits');
+    
+end
--- determine_match_sequences_plasmids.m
+++ determine_match_sequences_plasmids.m
@@ -19,29 +19,31 @@ for iPlasmid = 1:length(all_Plasmids)
     
     files_to_blast=extract_files_to_blast(PlasmidData_path,'specificPlasmid', specificPlasmid);
     
-    for iFile=1:length(files_to_blast)
-        
-        %Remove extension of blasted files
-        outputFile_name=remove_extension(files_to_blast{iFile});
-        
-        try% blasting
-            
-            [allBlastResults_sorted,bestScore_hits{iFile}] = blast_sequence(files_to_blast{iFile},path_data,PlasmidData_path);
-            specificPlasmid_results(iFile).Sequence = files_to_blast{iFile};
-            specificPlasmid_results(iFile).Result = allBlastResults_sorted;
-            
-        catch% catch blast not working
-            display (['BLAST failed with ' outputFile_name]);
-            
-        end
-        
-    end
-        
-    %Determine common best hits across BLASTS. Use when there are several
-    %plasmid sequences
-    commonHits = determine_commonHits_across_BLASTs(bestScore_hits);
+    blast_specific_plasmid(files_to_blast);
     
-    save(['../output_bioinformatics/BLAST_'  specificPlasmid], 'specificPlasmid_results','bestScore_hits','commonHits');
+%     for iFile=1:length(files_to_blast)
+%         
+%         %Remove extension of blasted files
+%         outputFile_name=remove_extension(files_to_blast{iFile});
+%         
+%         try% blasting
+%             
+%             [allBlastResults_sorted,bestScore_hits{iFile}] = blast_sequence(files_to_blast{iFile},path_data,PlasmidData_path);
+%             specificPlasmid_results(iFile).Sequence = files_to_blast{iFile};
+%             specificPlasmid_results(iFile).Result = allBlastResults_sorted;
+%             
+%         catch% catch blast not working
+%             display (['BLAST failed with ' outputFile_name]);
+%             
+%         end
+%         
+%     end
+%         
+%     %Determine common best hits across BLASTS. Use when there are several
+%     %plasmid sequences
+%     commonHits = determine_commonHits_across_BLASTs(bestScore_hits);
+%     
+%     save(['../output_bioinformatics/BLAST_'  specificPlasmid], 'specificPlasmid_results','bestScore_hits','commonHits');
 end
 
 toc
