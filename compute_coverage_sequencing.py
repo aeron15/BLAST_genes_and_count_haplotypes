e230d33c5c11a2644c36a02d11614917da09b48a
renan_macbookair <renan.escalante.chong@gmail.com>
2015-08-18 16:16:28 -0400

    Added all commands to compute coverage

--- compute_coverage_sequencing.py
+++ compute_coverage_sequencing.py
@@ -4,6 +4,7 @@ import sys, os
 '''
 
 Computes the coverage of the sequencing.
+Login to orchestra and run program from there.
 
 '''
 
@@ -18,10 +19,21 @@ command_loadBedtools = "module load seq/BEDTools/2.23.0"
 # Eventually replace the for loop where the names of the libraries are added.
 AlignmentFiles = ['r4H3L_yeast_genome','r3A10L_yeast_genome']
 
-#Now turn the sam files into bam files
 
 for samFile in AlignmentFiles:
 
+	#Convert sam files into bam files
 	comand_conversionSamToBam = "bsub -q sysbio_12h samtools view -bS -o /home/rae10/gal_seq/20121101/" + samFile + ".bam /home/rae10/gal_seq/20121101/" + samFile + ".sam ";
 	print comand_conversionSamToBam
 	#os.system(comand_conversionSamToBam)
+	
+	#Sort alignments
+	
+	command_sortBam = "bsub -q sysbio_12h samtools sort /home/rae10/gal_seq/20121101/" + samFile + ".bam /home/rae10/gal_seq/20121101/" + samFile + "_sorted"
+	print command_sortBam
+	
+	#Execute coverage calculation with option d
+	commandComputeCoverage = "genomeCoverageBed -ibam " + samFile + "_sorted.bam -g yeast_chr/yeast_genome.fna -bga > Bedtools_coverage_" + samFile
+	print commandComputeCoverage
+	
+	#Cut the column with the counts
